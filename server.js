a7983084d7e53472811546a3486ed69d4314960b
Marcelo Rigon <marcelo.rigon@gmail.com>
2019-01-27 02:02:45 -0200

    tweak for heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.set('port', (process.env.PORT || 3000));
+
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
@@ -12,6 +14,6 @@ app.use('/externals', express.static(__dirname + '/externals'));
 app.use('/', express.static(__dirname + '/public'));
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port: '+(process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
